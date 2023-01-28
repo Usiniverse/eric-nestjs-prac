c756c3f3f276b2868b6b00fb5a5648163ed6d2f4
Usiniverse <yushin@bookips.com>
2023-01-29 00:59:55 +0900

    study 3일차:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,22 @@
-import { Global, Module } from '@nestjs/common';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { EmailService } from './email/email.service';
-import { AppController } from './app.comtroller';
-import { CoreModule } from './common/coreModule';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { LoggerMiddleware } from './logger/logger.middleware';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import emailConfig from './config/emailConfig';
-import { validationSchema } from './config/validationSchema';
-
-console.log(__dirname);
-console.log(`${__dirname}/config/env/.${process.env.NODE_ENV}.env`);
+import { LoggerMiddleware2 } from './logger/logger.middleware2';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [
-    UsersModule,
-    EmailModule,
-    ConfigModule.forRoot({
-      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
-      load: [emailConfig],
-      isGlobal: true,
-      validationSchema,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService, ConfigService],
+  imports: [UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, LoggerMiddleware2)
+      .exclude({ path: '/users', method: RequestMethod.GET })
+      .forRoutes(UsersController);
+  }
+}
